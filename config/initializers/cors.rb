3f33585cfa7ce4815e6fca00daa311d61b922827
Redvan <redvanisation@gmail.com>
2020-06-10 23:56:20 -0400

    Update cors settings for Heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'localhost:3001', 'localhost:3000'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      # credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
