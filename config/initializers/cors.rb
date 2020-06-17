4263f8d946eae4e110dee68a9dc8e248840333cd
Redvan <redvanisation@gmail.com>
2020-06-16 20:52:27 -0400

    Update cors for heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', 'https://lucid-hypatia-0b87db.netlify.app/'
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
